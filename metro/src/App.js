f379d14249a2059fcbec79e8d25c1cdd26f7335c
Jung-Que <108854679+Jung-Que@users.noreply.github.com>
2023-05-24 10:23:11 +0900

    5_24_morning

--- metro/src/App.js
+++ metro/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import ClickGame from './ClickGame';
+import HorizonLine from './HorizonLine';
 
 // Home 컴포넌트
 const Home = () => {
@@ -23,39 +24,51 @@ const About = () => {
   return <h1>About 페이지</h1>;
 };
 
-// 메뉴 컴포넌트
 const Menu = () => {
   return (
-    <nav>
-      <ul>
+    <nav style={{ display: 'flex', alignItems: 'left', justifyContent: 'space-between' }}>
+      <div>
         <li>
-          <Link to="/" className="menu-link">Home</Link>
-        </li>
-        <li>
-          <Link to="/test" className="menu-link">Test</Link>
-        </li>
-        <li>
-          <Link to="/game" className="menu-link">Game</Link>
-        </li>
-        <li>
-          <Link to="/about" className="menu-link">About</Link>
-        </li>
-      </ul>
+            <Link to="/">
+              <img src="./logo.png" alt="로고" className="logo" style={{ width: 'auto', height: '100px' }} />
+            </Link>
+          </li>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <ul style={{ display: 'flex', alignItems: 'center', margin: 0 }}>
+          <li>
+            <Link to="/" className="menu-link">Home</Link>
+          </li>
+          <li>
+            <Link to="/test" className="menu-link">Test</Link>
+          </li>
+          <li>
+            <Link to="/game" className="menu-link">Game</Link>
+          </li>
+          <li>
+            <Link to="/about" className="menu-link">About</Link>
+          </li>
+        </ul>
+      </div>
+      <div style={{ paddingTop: "10px" ,paddingLeft: '0px', paddingRight: "80px"}}>
+        <img src="./gits.png" alt="이미지1" className="menu-image" style={{ width: '80px', height: '80px' }} />
+        <img src="./gits.png" alt="이미지2" className="menu-image" style={{ width: 'auto', height: '80px' }} />
+        <img src="./gits.png" alt="이미지3" className="menu-image" style={{ width: 'auto', height: '80px' }} />
+      </div>
     </nav>
   );
 };
+
+
 
 // App 컴포넌트
 const App = () => {
   return (
     <Router>
       <div>
-        <header>
-        <img src="./logo.png" alt="로고" className="logo"
-         style={{ width: 'auto', height: '100px' }} />
-        </header>
-        <Menu />
         
+        <Menu />
+        <HorizonLine text="OR"/>
         <Routes>
           <Route exact path="/" component={Home} />
           <Route path="/test" component={Test} />
